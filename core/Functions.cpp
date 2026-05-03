0ca46aadfa1403e9b179a42f63b471b77cdb0fb2
Joaquin-cabrera-d <joaquin.cabrera@alumnos.ucn.cl>
2026-05-03 13:34:08 -0400

    Added submenus to Functions.cpp

--- core/Functions.cpp
+++ core/Functions.cpp
@@ -1,1 +1,21 @@
+#pragma once
 
+void menuPlaylist(){
+	string opcion;
+	string input;
+	cout << "Opciones:" << endl;
+	cout << "S<num> - Saltar a la cancion seleccionada" << endl;
+	cout << "V - Volver al menu principal" << endl;
+}
+
+void menuListado(){
+	string opcion;
+	string input;
+	cout << "Opciones:" << endl;
+	cout << "R<num> - Reproducir cancion seleccionada" << endl;
+	cout << "A<num> - Agregar cancion seleccionada al final de la lista de reproduccion actual" << endl;
+	cout << "N - Agregar cancion al registro de canciones" << endl;
+	cout << "D<num> - Eliminar cancion seleccionada" << endl;
+	cout << "V - Volver al menu principal" << endl;
+	
+}
