d04216c4a05ed13c02b51de84352fcbc98cd7d13
Dylan Cortes <dylancortespasten@gmail.com>
2026-05-03 23:08:03 -0400

    Create Lista.h
    este es el lista.h

---
+++ classes/Lista.h
@@ -0,0 +1,44 @@
+//
+// Created by chapuza on 03-05-2026.
+//
+
+#pragma once
+#include "Cancion.h"
+
+class Lista {
+private:
+    Cancion* principio;
+    Cancion* cursor;
+    Cancion* cancionActual;
+    bool reproduciendo;
+    bool modoAleatorio;
+    int modoRepeticion;
+    int totalCanciones;
+
+public:
+    Lista();
+    ~Lista();
+
+    bool Vacia();
+    void Agregar(Cancion* cancion);
+    void Print();
+
+    // Control de reproducción
+    void reproducirPausar();
+    void siguiente();
+    void anterior();
+    void saltarACancion(int posicion);
+
+    // Modos especiales
+    void toggleAleatorio();
+    void toggleRepeticion();
+    void mezclarLista();
+
+    // Mostrar información
+    void mostrarEstadoActual();
+    void mostrarListaReproduccion();
+    void mostrarTodasCanciones();
+
+    // Getter
+    Cancion* getCancionActual() { return cancionActual; }
+};
